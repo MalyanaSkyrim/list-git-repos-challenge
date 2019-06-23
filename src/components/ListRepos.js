94c28ac1602e884f98b8e18dd3b555197de5a77a
malyana <malyanamohamed1@gmail.com>
2019-06-23 20:43:47 +0100

    adapt response data with needed data

--- src/components/ListRepos.js
+++ src/components/ListRepos.js
@@ -17,7 +17,17 @@ class ListRepos extends Component {
         const res = await axios.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc');
         
         console.log(res);
-        const repos = res.data.items;
+        const repos = res.data.items
+                .map(repo => ({
+                    title:repo.name,
+                    description:repo.description,
+                    avatarUrl:repo.owner.avatar_url,
+                    nbStars: repo.stargazers_count,
+                    nbIssues:repo.open_issues_count,
+                    timeInterval:repo.timeInterval,
+                    ownerName:repo.owner.login,
+                    repoUrl:repo.repoUrl
+                }));
 
         this.setState({repos});
 
